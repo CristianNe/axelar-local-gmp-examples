88dccb07183b187170d3b4a0fcb64e428a7f8774
Cristian Neufuss <cristian.neufuss@hotmail.de>
2022-05-23 20:14:32 +0200

    fixed TransferCrossChain task

--- examples/test-cross-chain/index.js
+++ examples/test-cross-chain/index.js
@@ -3,16 +3,17 @@
 const { getDefaultProvider, Contract, constants: { AddressZero } } = require('ethers');
 const { utils: { deployContract }} = require('@axelar-network/axelar-local-dev');
 
-const Executor = require('../../build/Executor.json');
+const TestCrossChain = require('../../build/TestCrossChain.json');
 const Gateway = require('../../build/IAxelarGateway.json');
 const IERC20 = require('../../build/IERC20.json');
 
-async function deploy(chain, wallet){
-    console.log(`Deploying TransferCrossChainExecutable for ${chain.name}`);
-    const contract = await deployContract(wallet, Executor, [chain.gateway, chain.gasReceiver]);
-    chain.executor = contract.address;
-    console.log(`Deployed TransferCrossChainExecutable for ${chain.name} at ${chain.executor}.`);
+async function deploy(chain, wallet) {
+    console.log(`Deploying TestCrossChain for ${chain.name}.`);
+    const contract = await deployContract(wallet, TestCrossChain, [chain.gateway, chain.gasReceiver]);
+    chain.testCrossChain = contract.address;
+    console.log(`Deployed DistributionExecutable for ${chain.name} at ${chain.testCrossChain}.`);
 }
+
 
 async function test(chains, wallet, options) {
     const args = options.args || [];
@@ -26,7 +27,7 @@ async function test(chains, wallet, options) {
     for(const chain of [source, destination]) {
         const provider = getDefaultProvider(chain.rpc);
         chain.wallet = wallet.connect(provider);
-        chain.contract = new Contract(chain.executor, Executor.abi, chain.wallet);
+        chain.contract = new Contract(chain.testCrossChain, TestCrossChain.abi, chain.wallet);
         chain.gateway = new Contract(chain.gateway, Gateway.abi, chain.wallet);
         const usdcAddress = chain.gateway.tokenAddresses('aUSDC');
         chain.usdc = new Contract(usdcAddress, IERC20.abi, chain.wallet);
@@ -34,8 +35,7 @@ async function test(chains, wallet, options) {
     
     async function print() {
         for(const account of accounts) {
-            console.log(`${source.name}: ${account} has ${await source.usdc.balanceOf(account)/1e6} aUSDC`)
-            console.log(`${destination.name}: ${account} has ${await destination.usdc.balanceOf(account)/1e6} aUSDC`)
+            console.log(`${account} has ${await destination.usdc.balanceOf(account)/1e6} aUSDC`)
         }
     }
     function sleep(ms) {
@@ -48,39 +48,23 @@ async function test(chains, wallet, options) {
     await print();
 
     const gasLimit = 3e6;
-    console.log('Get Gas Price...');
     const gasPrice = await getGasPrice(source, destination, AddressZero);
-    console.log('Gas Price:', gasPrice);
     
-    console.log('Get Balance of %s...', accounts[0]);
     const balance = BigInt(await destination.usdc.balanceOf(accounts[0]));
-    console.log('Balance: ', balance);
-
-    console.log('Approve Executor contract on source chain to spent amount...');
     await (await source.usdc.approve(
         source.contract.address,
         amount,
     )).wait();
-
-    console.log('Call tranferTokenCrossChain function of Executor contract');
-    console.log('destination chain: ', destination.name);
-    console.log('destination chain contract address: ', destination.executor);
-    console.log('recipient address: ', accounts[0]);
-    console.log('amount: ', amount);
-    
-    await (await source.contract.transferTokenCrossChain(
+    await (await source.contract.sendToFirst(
         destination.name,
-        destination.executor,
-        accounts[0], 
+        destination.testCrossChain,
+        accounts, 
         'aUSDC',
         amount,
         {value: BigInt(Math.floor(gasLimit * gasPrice))}
     )).wait();
-    
-    console.log('Call finished...');
-    
     while(BigInt(await destination.usdc.balanceOf(accounts[0])) == balance) {
-        await sleep(5000);
+        await sleep(2000);
     }
 
     console.log('--- After ---');
--- examples/transfer-tokens-cross-chain/Executor.sol
+++ examples/transfer-tokens-cross-chain/Executor.sol
@@ -1,25 +1,24 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-import {IAxelarGasReceiver} from "@axelar-network/axelar-cgp-solidity/src/interfaces/IAxelarGasReceiver.sol";
+import {AxelarGasReceiver} from "@axelar-network/axelar-cgp-solidity/src/gas-receiver/AxelarGasReceiver.sol";
 import {IAxelarExecutable} from "@axelar-network/axelar-cgp-solidity/src/interfaces/IAxelarExecutable.sol";
 import { IERC20 } from '@axelar-network/axelar-cgp-solidity/src/interfaces/IERC20.sol';
-import "hardhat/console.sol";
 
 contract Executor is IAxelarExecutable {
     // Axerlar Network only approves messages
     // We pay estimated gas costs upront on source chain
     // to use Axelars relay service Axelar Gas Receiver to execute the approved message
-    IAxelarGasReceiver gasReceiver;
+    AxelarGasReceiver gasReceiver;
 
     constructor(address _gateway, address _gasReceiver) IAxelarExecutable(_gateway) {
-        gasReceiver = IAxelarGasReceiver(_gasReceiver);
+        gasReceiver = AxelarGasReceiver(_gasReceiver);
     }
 
     function transferTokenCrossChain(
         string memory destinationChain,
-        string memory contractAddress,
-        string memory recipientAddress,
+        string memory destinationAddress,
+        address recipientAddress,
         string memory symbol,
         uint256 amount
     ) external payable {
@@ -31,18 +30,12 @@ contract Executor is IAxelarExecutable {
         // encode recipient address in payload to be passed to contract on destination chain
         bytes memory payload = abi.encode(recipientAddress);
 
-        console.log('tokenAddress: ', tokenAddress);
-        console.log('contractAddress destination chain: ', contractAddress);
-        console.log('recipientAddress: ', recipientAddress);
-        console.log('amount:', amount);
-        console.log('msg.value:', msg.value);
-
         if(msg.value > 0){
             // pay Gas Receiver upfront with native token for execution
             gasReceiver.payNativeGasForContractCallWithToken{value: msg.value}(
                 address(this), // sender
                 destinationChain,
-                contractAddress, // address of AxelarExecutable Contract on destination chain
+                destinationAddress, // address of AxelarExecutable Contract on destination chain
                 payload, // contains the address of final recipient
                 symbol, // 
                 amount,
@@ -51,21 +44,20 @@ contract Executor is IAxelarExecutable {
         }
         // call callContractWithToken function of the user's source chain gateway contract
         // the gateway will then call _executeWithToken function on the destination chain
-        gateway.callContractWithToken(destinationChain, contractAddress, payload, symbol, amount);
+        gateway.callContractWithToken(destinationChain, destinationAddress, payload, symbol, amount);
 
     }
 
     function _executeWithToken(
         string memory, 
         string memory,
-        bytes memory payload, 
+        bytes calldata payload, 
         string memory symbol, 
         uint256 amount
         ) internal override {
             // decode recipient
             address recipient = abi.decode(payload, (address));
-            console.log("recipientAddress(decoded):", recipient);
-            
+
             // get ERC-20 address from gateway
             address tokenAddress = gateway.tokenAddresses(symbol);
             IERC20(tokenAddress).transfer(recipient, amount);
